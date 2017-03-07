90a192ebdd7ef60f4ee1eb4851815c682490577e
Jacky Tang <jackytck@gmail.com>
2017-03-07 22:48:58 +0800

    :package: add common components

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ import {
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
+import { Header } from './components/common'
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText='Tech Stack' />
+      </View>
     </Provider>
   )
 }
---
+++ src/components/common/Spinner.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react'
+import {
+  View,
+  ActivityIndicator
+} from 'react-native'
+
+const Spinner = ({ size }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
+  )
+}
+
+Spinner.propTypes = {
+  size: PropTypes.string
+}
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
+export default Spinner
