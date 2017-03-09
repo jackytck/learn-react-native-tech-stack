370172062a93a6c9d03eb8b4ea7fa06b37f9eb8f
Jacky Tang <jackytck@gmail.com>
2017-03-10 00:10:09 +0800

    :construction: add selectedLibraryId state

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,23 +1,49 @@
 import React, { Component, PropTypes } from 'react'
-import { Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback
+} from 'react-native'
+import { connect } from 'react-redux'
 import { CardSection } from './common'
+import * as actions from '../actions'
 
 class ListItem extends Component {
+  renderDescription () {
+    const { library, selectedLibraryId } = this.props
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      )
+    }
+  }
+
   render () {
     const { titleStyle } = styles
+    const { id, title } = this.props.library
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
 
 ListItem.propTypes = {
-  library: PropTypes.object
+  library: PropTypes.object,
+  selectedLibraryId: PropTypes.number,
+  selectLibrary: PropTypes.func
 }
 
 const styles = {
@@ -27,4 +53,10 @@ const styles = {
   }
 }
 
-export default ListItem
+const mapStateToProps = state => {
+  return {
+    selectedLibraryId: state.selectedLibraryId
+  }
+}
+
+export default connect(mapStateToProps, actions)(ListItem)
